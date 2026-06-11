9090db55c8954c1e0c813b78ceb3362866a8074e
CoutoDev <c.svitor@yahoo.com.br>
2026-06-11 19:09:46 -0300

    feat: add music, final image and fix object weight

--- game.js
+++ game.js
@@ -20,11 +20,11 @@ async function loadSVG(svgFileName = '') {
 const itemTypes = []
 // Cute SVG icons as data URLs
 const generateIcons = async () => {
-    const lilo =  await loadSVG('lilosvg')
-    const ze =  await loadSVG('zesvg')
-    const usVapor =  await loadSVG('us-vapor')
-    const joyTrip =  await loadSVG('joy-trip')
-    const joyPhoto =  await loadSVG('joy-phototime')
+    const lilo = await loadSVG('lilosvg')
+    const ze = await loadSVG('zesvg')
+    const usVapor = await loadSVG('us-vapor')
+    const joyTrip = await loadSVG('joy-trip')
+    const joyPhoto = await loadSVG('joy-phototime')
 
     return {
         heart: `data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="%23ff6b9d" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>')}`,
@@ -262,12 +262,12 @@ function startGame() {
 
     generateIcons().then(svgIcons => {
         itemTypes.push(
-            { type: 'heart', icon: svgIcons.heart, points: 1, weight: 0.3 },
-            { type: 'lilo', icon: svgIcons.lilo, points: 3, weight: 0.2 },
-            { type: 'ze', icon: svgIcons.ze, points: 3, weight: 0.2 },
-            { type: 'joyPhoto', icon: svgIcons.joyPhoto, points: 10, weight: 0.15 },
-            { type: 'joyTrip', icon: svgIcons.joyTrip, points: 15, weight: 0.25 },
-            { type: 'special', icon: svgIcons.special, points: 20, weight: 0.1 }
+            { type: 'heart', icon: svgIcons.heart, points: 1, weight: 0.23 },
+            { type: 'lilo', icon: svgIcons.lilo, points: 3, weight: 0.15 },
+            { type: 'ze', icon: svgIcons.ze, points: 3, weight: 0.15 },
+            { type: 'joyPhoto', icon: svgIcons.joyPhoto, points: 10, weight: 0.11 },
+            { type: 'joyTrip', icon: svgIcons.joyTrip, points: 15, weight: 0.19 },
+            { type: 'special', icon: svgIcons.special, points: 20, weight: 0.17 }
         )
         resizeCanvas();
         animationId = requestAnimationFrame(update);
